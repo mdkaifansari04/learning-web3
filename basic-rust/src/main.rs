1c3a18d9d1928382cec5762a1c6d1995fe394880
Md Kaif Ansari <amdkaif843@gmail.com>
2025-06-29 23:19:46 +0530

    feat: implement fibinaci series

--- basic-rust/src/main.rs
+++ basic-rust/src/main.rs
@@ -1,13 +1,27 @@
 fn main() {
     let ans = is_even(12);
-    if ans{
-        print!("Number is even");
-    }else{
-        print!("Number is odd");
-    }
+    fiboo(5);
 }
 
+// conditionals
 fn is_even(num: i32) ->bool{
-    if num % 2 == 0 {  return true;}
-    return false;
+    return num % 2 == 0;
+}
+
+fn fiboo(n: i32) -> (){
+    if n>1 {
+        let mut first=0;
+        let mut second = 1;
+        let mut i=2;
+        print!("{} {}", first, second);
+        while i < n { 
+            let third = first+second;
+            print!(" {}", third);
+            first = second;
+            second = third;
+            i += 1;
+        }
+    }else{
+        println!("Enter a valid number");
+    }
 }
