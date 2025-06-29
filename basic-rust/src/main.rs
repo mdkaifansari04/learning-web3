c30ac12dbcce0d39931e75712ea326b4760b5f9f
Md Kaif Ansari <amdkaif843@gmail.com>
2025-06-29 22:54:43 +0530

    feat: conditional statements

--- basic-rust/src/main.rs
+++ basic-rust/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let ans = is_even(12);
+    if ans{
+        print!("Number is even");
+    }else{
+        print!("Number is odd");
+    }
+}
+
+fn is_even(num: i32) ->bool{
+    if num % 2 == 0 {  return true;}
+    return false;
 }
