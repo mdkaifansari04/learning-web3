4d57fe9f4f47b99ffa122a908b893b764cd4260c
Md Kaif Ansari <amdkaif843@gmail.com>
2025-06-29 23:43:34 +0530

    chore: update main

--- basic-rust/src/main.rs
+++ basic-rust/src/main.rs
@@ -1,11 +1,4 @@
-fn main() {
-    let ans = is_even(12);
-    fiboo(5);
-    print_series(10);
-    let name = String::from("Kaif"); 
-    let count = get_string_length(&name);
-    println!("The length of string is :{} ",count )
-}
+
 
 // conditionals
 fn is_even(num: i32) ->bool{
@@ -41,4 +34,40 @@ fn print_series(num: u32) -> (){
 //string methods 
 fn get_string_length(s : &str)->usize{
     s.chars().count()
+}
+
+
+
+
+// structs in rust
+// structs are basically the objects in JS
+
+
+struct User {
+    active : bool,
+    email : String,
+    name :String,
+    sign_in_count : u64,
+}
+
+fn create_user(name: String, active : bool, email: String) -> (){
+    let user = User{
+        active,
+        name,
+        email,
+        sign_in_count : 1
+    };
+    print!("User created : {:?}", user.name);
+}
+
+
+fn main() {
+    let _ans = is_even(12);
+    fiboo(5);
+    print_series(10);
+    let name = String::from("Kaif"); 
+    let count = get_string_length(&name);
+    println!("The length of string is :{} ",count);
+
+    create_user(String::from("kaif"), true, String::from("kaif@demo.com"));
 }
