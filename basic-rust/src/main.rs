9a2d846ec3af3b8f00b8cfa2c4ca38832ee7b426
Md Kaif Ansari <amdkaif843@gmail.com>
2025-06-29 23:23:42 +0530

    feat: loop

--- basic-rust/src/main.rs
+++ basic-rust/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     let ans = is_even(12);
     fiboo(5);
+    print_series(10);
 }
 
 // conditionals
@@ -23,5 +24,13 @@ fn fiboo(n: i32) -> (){
         }
     }else{
         println!("Enter a valid number");
+    }
+}
+
+// for loop
+fn print_series(num: u32) -> (){
+    println!();
+    for i in 1..=num  {
+        print!("{} ", i);
     }
 }
